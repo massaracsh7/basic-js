3ce664da5535c8fafa3ce46cb2db7d48493c5f69
Your Name <you@example.com>
2023-04-12 11:49:31 +0300

    feat: solve what season

--- src/what-season.js
+++ src/what-season.js
@@ -11,8 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if (date instanceof Date == false || Object.keys(date).length){
+    throw new Error('Invalid date!');
+  }
+  let month = date.getMonth();
+  if (month >= 2 && month <= 4) {
+    return 'spring';
+  }
+  if (month >= 5 && month <= 7) {
+    return 'summer';
+  }
+  if (month >= 8 && month <= 10) {
+    return 'autumn';
+  } else {
+    return 'winter';
+  }
+
   // remove line with error and write your code here
 }
 
